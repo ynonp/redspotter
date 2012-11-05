2f3aa37364e811da043a2e223cbfd51a38f6114b
ynonp <ynonperek@gmail.com>
2012-11-05 13:11:11 +0200

    with transitions

--- app.js
+++ app.js
@@ -2,21 +2,27 @@
 
     var app = {};
 
-    var container = document.querySelector('#container');
+    var next_page = document.querySelector('.page.next');
+    var current_page = document.querySelector('.page.current');
 
     var g = new global.app.Game(
         global.app.Score,
         global.app.Winner,
         global.app.Looser,
-        container
+        next_page.querySelector('#container')
     );
 
-    var starter = new global.app.Starter( app, container );
+    var starter = new global.app.Starter( app, current_page );
 
     starter.render();
+    g.reshuffle();
 
     app.start_game = function() {
-        g.reshuffle();
+        next_page.classList.remove('next');
+        next_page.classList.add('current');
+
+        current_page.classList.remove('current');
+        current_page.classList.add('previous');
     };
 
 
